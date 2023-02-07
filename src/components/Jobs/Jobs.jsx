b8ff955d1914fe34375bb2be02903903880a2336
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-07 18:04:22 -0500

    Jobs page

---
+++ src/components/Jobs/Jobs.jsx
@@ -0,0 +1,96 @@
+import {motion} from "framer-motion";
+import "./Jobs.scss";
+import jobsData from "../../data/jobs";
+
+const variantsLi = {
+    open: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            y: {stiffness: 1000, velocity: -100}
+        }
+    },
+    closed: {
+        y: 50,
+        opacity: 0,
+        transition: {
+            y: {stiffness: 1000, velocity: -100}
+        }
+    }
+};
+
+const list = {
+    visible: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.3,
+            delay: 1
+        },
+    },
+    hidden: {
+        opacity: 0,
+        transition: {
+            when: "afterChildren",
+        },
+    },
+}
+const item = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -100 },
+}
+
+const listOfAsideItems = jobsData.jobs.map((item, index) =>
+    <li key={`li-${index}`} className="Jobs-menu-item">
+        <motion.div
+            variants={variantsLi}
+            key={index}
+            className="Jobs-menu-item-wrapper"
+        >
+            <div className="Jobs-menu-item-header">
+                {item.header}
+            </div>
+            <div className="Jobs-menu-item-position">
+                {item.position}
+            </div>
+            <div className="Jobs-menu-item-dates">
+                {item.dates}
+            </div>
+            <div className="Jobs-menu-item-details">
+                {item.details}
+            </div>
+
+        </motion.div>
+    </li>
+);
+
+const Jobs = () => {
+    return (
+        <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={list}
+            className="Jobs"
+        >
+            <motion.div
+                className="heading"
+                variants={item}
+            >
+                {jobsData.header}
+            </motion.div>
+            <motion.div
+                className="content"
+                variants={item}
+                dangerouslySetInnerHTML={{__html: jobsData.content}}>
+            </motion.div>
+            <div className="Jobs-menu-aside">
+                <motion.ul
+                    variants={item}
+                >
+                    {listOfAsideItems}
+                </motion.ul>
+            </div>
+        </motion.div>
+    )
+}
+export default Jobs;
---
+++ src/data/jobs.js
@@ -0,0 +1,19 @@
+const jobsData = {
+    header: 'Job Experience',
+    content: '<p>Front-end developer with full experience building and designing responsive driven websites and web applications, using HTML5, CSS3, JavaScript and different web scripting technologies, web standards and project management tools and principles of Atomic Design. Enthusiastic web professional with deep design jobss. Always exploring new coding techniques and environments. Experience in the full life cycle of the web design process including requirements, definition, interface design, implementation, coding, testing and maintenance.</p>',
+    jobs: [
+        {
+            header: 'NCLH - Norwegian Cruise Line',
+            position: 'Senior UI/UX Front-end developer.',
+            dates: 'July 2018 - Present',
+            details: 'UI/UX development using Atomic Design patterns over Google Closure Templates. Working with Vue.js components and developing DRY components. Translate mock-ups designs into pixel perfect interactive Modules and design guide implementation. Use of Webpack, ES6, Jquery, SASS. Integration of Vue.js with Soy files (Google Closure)'
+        },
+        {
+            header: 'Everymundo.com',
+            position: 'Front-end developer',
+            dates: 'July 2018 - Present',
+            details: 'Translate airline clients mock-ups designs into pixel perfect accessible and SEO ready websites. Airline clients feedback including Mega Menus, animated effects, special booking. Development of a LESS and TWIG theme to allow faster deliverables. Working with Bootstrap, Vanilla JS, Jquery, Less, GIT, Google Insights, SemRush. Implementation of Progressive Web Apps (PWA) to enhance airline website performance along with a better UX. Creation of React Components replacing old legacy modules. React component testing using Karma, Mocha and Chai.'
+        },
+    ]
+};
+export default jobsData;
