dfbb616b1c3a4f941e340a0d857fefb9e956d930
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-07 15:21:35 -0500

    Completing About page content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,53 @@
 import {motion} from "framer-motion";
 import "./About.scss";
+import aboutData from "../../data/about";
+
+const list = {
+    visible: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.3,
+            delay: 1
+        },
+    },
+    hidden: {
+        opacity: 0,
+        transition: {
+            when: "afterChildren",
+        },
+    },
+}
+const item = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -100 },
+}
 
 const About = () => {
     return (
         <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{opacity: 0, y:-20}}
-            transition={{ duration: 1}}
+            initial="hidden"
+            animate="visible"
+            variants={list}
+            className="About"
         >
-        <div className="About">
-            <div className="heading">
-                About
-            </div>
-        </div>
+            <motion.div
+                className="heading"
+                variants={item}
+            >
+                {aboutData.header}
+            </motion.div>
+            <motion.div
+                className="content"
+                variants={item}
+                dangerouslySetInnerHTML={{__html: aboutData.content}}>
+            </motion.div>
+            <motion.div
+                className="aside"
+                variants={item}
+            >
+                <a href="#">Donwload resume</a>
+            </motion.div>
         </motion.div>
     )
 }
---
+++ src/data/about.js
@@ -0,0 +1,5 @@
+const aboutData = {
+    header: 'About me',
+    content: '<p>Front-end developer with full experience building and designing responsive driven websites and web applications, using HTML5, CSS3, JavaScript and different web scripting technologies, web standards and project management tools and principles of Atomic Design. Enthusiastic web professional with deep design skills. Always exploring new coding techniques and environments. Experience in the full life cycle of the web design process including requirements, definition, interface design, implementation, coding, testing and maintenance.</p>',
+};
+export default aboutData;
