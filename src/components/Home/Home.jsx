26267967a412669644c9ff337bd807bfa96a72f6
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-07 15:21:59 -0500

    Home page delay animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{opacity: 0, y:-20}}
-            transition={{ duration: 1 }}
+            transition={{ duration: 1, delay: 1 }}
         >
             <div className="Home">
                 <div className="heading">
