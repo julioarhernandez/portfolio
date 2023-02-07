d51a1111d6f4388f0410aeba78243e3a10214599
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-07 15:55:26 -0500

    Home page layout and data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,95 @@
 import {motion} from "framer-motion";
 import "./Home.scss";
+import homeData from "../../data/home";
 
+const list = {
+    visible: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.3,
+            delay: 1
+        },
+    },
+    hidden: {
+        opacity: 0,
+        transition: {
+            when: "afterChildren",
+        },
+    },
+};
+
+const variantsLi = {
+    open: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            y: {stiffness: 1000, velocity: -100}
+        }
+    },
+    closed: {
+        y: 50,
+        opacity: 0,
+        transition: {
+            y: {stiffness: 1000, velocity: -100}
+        }
+    }
+};
+
+const item = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -100 },
+}
+
+const listOfAsideItems = homeData.links.map((item, index) =>
+    <li key={`li-${index}`} className="Home-menu-item">
+        <motion.div
+            variants={variantsLi}
+            key={index}
+            className="Home-menu-item-wrapper"
+        >
+            <a href={item.link} target="_blank">{item.label}</a>
+        </motion.div>
+    </li>
+);
 
 const Home = () => {
     return (
         <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{opacity: 0, y:-20}}
-            transition={{ duration: 1, delay: 1 }}
+            initial="hidden"
+            animate="visible"
+            variants={list}
+            className="Home"
         >
-            <div className="Home">
-                <div className="heading">
-                    Home
-                </div>
-                .
+            <motion.div
+                className="heading heading-transparent"
+                variants={item}
+            >
+                <span>{homeData.headerTitle}</span>
+                <span className="heading-highlighted">{homeData.headerTitleName}</span>
+
+            </motion.div>
+            <motion.div
+                className="heading"
+                variants={item}
+            >
+                {homeData.headerSubtitle}
+
+            </motion.div>
+            <motion.div
+                className="content"
+                variants={item}
+                dangerouslySetInnerHTML={{__html: homeData.content}}>
+            </motion.div>
+            <div className="Home-menu-aside">
+                <motion.ul
+                    variants={item}
+                >
+                    {listOfAsideItems}
+                </motion.ul>
             </div>
         </motion.div>
+
     )
 }
 export default Home;
---
+++ src/data/home.js
@@ -0,0 +1,21 @@
+const homeData = {
+    headerTitle: 'Hi Im',
+    headerTitleName: 'Fulano',
+    headerSubtitle: 'front-end developer',
+    content: '<p>Experience in the full life cycle of the web developtment process including requirements, definition, interface design, implementation, testing and maintenance. </p>',
+    links: [
+        {
+            label: 'Skills',
+            link: 'skills/',
+        },
+        {
+            label: 'Job Experience',
+            link: 'jobs/',
+        },
+        {
+            label: 'Contact me',
+            link: 'contact/',
+        },
+    ]
+};
+export default homeData;
