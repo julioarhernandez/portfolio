3b53c1b80b13274b6d2bb09b5a49cfdebd6b3b0a
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-16 15:05:01 -0500

    Refactor App code

--- src/App.js
+++ src/App.js
@@ -2,30 +2,11 @@ import React, {useEffect, useState} from "react";
 import { useOutlet, useLocation } from "react-router-dom";
 import {motion as m} from "framer-motion";
 import {MenuContext} from "./contexts/contexts";
+import appMainTransition from "./motions/appMainTransition";
 
 import Nav from "./components/Nav/Nav";
 
 import './App.scss';
-
-const variantsMenu = {
-    open: {
-        display: 'block',
-        opacity: 1,
-        transition: {
-            when: "beforeChildren",
-            duration: 1,
-            delay: 0.5
-        }
-    },
-    closed: {
-        opacity: 0,
-        transition: {
-            when: "afterChildren",
-            duration: 0.3,
-        },
-        transitionEnd: { display: "none" }
-    },
-}
 
 function App() {
     const currentOutlet = useOutlet();
@@ -33,17 +14,14 @@ function App() {
     const { pathname } = useLocation();
   return (
       <MenuContext.Provider value={[menuOpen, setMenuOpen]}>
-            <div className="App">
-              <header className="App-header">
-                  <Nav activePath={pathname}/>
-              </header>
-                <m.div className="App-main"
-                    animate={menuOpen ? "closed" : "open"}
-                    key="openIcon"
-                    variants={variantsMenu}>
-                          {currentOutlet && React.cloneElement(currentOutlet, { key: pathname })}
-                </m.div>
-            </div>
+        <div className="App">
+          <header className="App-header">
+              <Nav activePath={pathname}/>
+          </header>
+            <m.div className="App-main" animate={menuOpen ? "closed" : "open"} key="openIcon" variants={appMainTransition}>
+                {currentOutlet && React.cloneElement(currentOutlet, { key: pathname })}
+            </m.div>
+        </div>
       </MenuContext.Provider>
   );
 }
