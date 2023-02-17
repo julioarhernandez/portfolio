6e7723a4f8846379108cccf8a0c0f98b548d14c4
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-17 00:36:04 -0500

    Fonts preloading and splash screen added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { useOutlet, useLocation } from "react-router-dom";
+import {useOutlet, useLocation} from "react-router-dom";
 import {motion as m} from "framer-motion";
 import {MenuContext} from "./contexts/contexts";
 import appMainTransition from "./motions/appMainTransition";
@@ -10,20 +10,30 @@ import './App.scss';
 
 function App() {
     const currentOutlet = useOutlet();
+    const [isLoading, setLoading] = useState(true);
     const [menuOpen, setMenuOpen] = useState(false);
-    const { pathname } = useLocation();
-  return (
-      <MenuContext.Provider value={[menuOpen, setMenuOpen]}>
-        <div className="App">
-          <header className="App-header">
-              <Nav activePath={pathname}/>
-          </header>
-            <m.div className="App-main" animate={menuOpen ? "closed" : "open"} key="openIcon" variants={appMainTransition}>
-                {currentOutlet && React.cloneElement(currentOutlet, { key: pathname })}
-            </m.div>
-        </div>
-      </MenuContext.Provider>
-  );
+    const {pathname} = useLocation();
+
+    useEffect(() => {
+        const el = document.querySelector(".loader-container");
+        if (el) {
+            el.remove();
+            setLoading(!isLoading);
+        }
+    }, []);
+
+    return (
+        <MenuContext.Provider value={[menuOpen, setMenuOpen]}>
+            <div className="App">
+                <header className="App-header">
+                    <Nav activePath={pathname}/>
+                </header>
+                <m.div className="App-main" animate={menuOpen ? "closed" : "open"} key="openIcon" variants={appMainTransition}>
+                    {currentOutlet && React.cloneElement(currentOutlet, {key: pathname})}
+                </m.div>
+            </div>
+        </MenuContext.Provider>
+    );
 }
 
 export default App;
